629ba4c607084e8218e8a5f7221031113d654e8c
OleksandrKulchytskyi <sashaas@inbox.ru>
2014-03-31 13:40:05 +0300

    Fixed issue with handling row index and user votes marks in roomactivity View

--- Server/WebSignalR/WebSignalR/App_Start/SignalRConfig.cs
+++ Server/WebSignalR/WebSignalR/App_Start/SignalRConfig.cs
@@ -6,41 +6,57 @@ using System.Web.Routing;
 
 namespace WebSignalR.App_Start
 {
-    public static class SignalRConfig
-    {
-        public static void Register(IDependencyResolver resolver)
-        {
-            HubConfiguration hubConfig = new HubConfiguration
-            {
-                Resolver = resolver,
-                EnableJavaScriptProxies = true,
-                EnableDetailedErrors = true
-            };
+	public static class SignalRConfig
+	{
+		public static void Register(IDependencyResolver resolver)
+		{
+			HubConfiguration hubConfig = new HubConfiguration
+			{
+				Resolver = resolver,
+				EnableJavaScriptProxies = true,
+				EnableDetailedErrors = true
+			};
 
-            if (bool.Parse(System.Configuration.ConfigurationManager.AppSettings["Redis.Enable"]))
-            {
-                string server = System.Configuration.ConfigurationManager.AppSettings["Redis.Server"];
-                string port = System.Configuration.ConfigurationManager.AppSettings["Redis.Port"];
-                string password = System.Configuration.ConfigurationManager.AppSettings["Redis.Password"];
-                var scaleout = new RedisScaleoutConfiguration(server, int.Parse(port), password, "WebSignalR");
-                GlobalHost.DependencyResolver.UseRedis(scaleout);
-            }
+			if (bool.Parse(System.Configuration.ConfigurationManager.AppSettings["Redis.Enable"]))
+			{
+				string server = System.Configuration.ConfigurationManager.AppSettings["Redis.Server"];
+				string port = System.Configuration.ConfigurationManager.AppSettings["Redis.Port"];
+				string password = System.Configuration.ConfigurationManager.AppSettings["Redis.Password"];
+				var scaleout = new RedisScaleoutConfiguration(server, int.Parse(port), password, "WebSignalR");
+				GlobalHost.DependencyResolver.UseRedis(scaleout);
+			}
 
-            RouteTable.Routes.MapHubs(hubConfig);
+			RouteTable.Routes.MapHubs(hubConfig);
 
-            IConfigurationManager configManager = resolver.Resolve<IConfigurationManager>();
-            configManager.ConnectionTimeout = TimeSpan.FromSeconds(30);
-            configManager.DisconnectTimeout = TimeSpan.FromSeconds(30);
+			#region temp validation
+			//try
+			//{
+			//	object bus = GlobalHost.DependencyResolver.Resolve<Microsoft.AspNet.SignalR.Messaging.IMessageBus>();
+			//	if (bus != null)
+			//	{
 
-            IHubPipeline hubPipeline = resolver.Resolve<IHubPipeline>();
-            hubPipeline.AddModule(new Hubs.Pipelines.LogErrorPipelineModule());
+			//	}
+			//}
+			//catch (Exception)
+			//{
 
-            System.Web.Http.GlobalConfiguration.Configuration.Formatters.JsonFormatter.
-                SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+			//	throw;
+			//} 
+			#endregion
 
-            //this was disappear since v 1.0,this feature is turned on by defult in v1.0 >
-            //pipeline.EnableAutoRejoiningGroups();
-            //RouteTable.Routes.MapHubs();
-        }
-    }
+			IConfigurationManager configManager = resolver.Resolve<IConfigurationManager>();
+			configManager.ConnectionTimeout = TimeSpan.FromSeconds(30);
+			configManager.DisconnectTimeout = TimeSpan.FromSeconds(30);
+
+			IHubPipeline hubPipeline = resolver.Resolve<IHubPipeline>();
+			hubPipeline.AddModule(new Hubs.Pipelines.LogErrorPipelineModule());
+
+			System.Web.Http.GlobalConfiguration.Configuration.Formatters.JsonFormatter.
+				SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+			//this was disappear since v 1.0,this feature is turned on by defult in v1.0 >
+			//pipeline.EnableAutoRejoiningGroups();
+			//RouteTable.Routes.MapHubs();
+		}
+	}
 }
