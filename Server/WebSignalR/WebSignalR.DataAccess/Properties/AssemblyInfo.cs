a932cc552041b7e7977bc3c8eceb96e2c36b8daa
OleksandrKulchytskyi <sashaas@inbox.ru>
2014-04-01 18:01:15 +0300

    New versioning file structure

--- Server/WebSignalR/WebSignalR.DataAccess/Properties/AssemblyInfo.cs
+++ Server/WebSignalR/WebSignalR.DataAccess/Properties/AssemblyInfo.cs
@@ -20,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("33fe6963-7d93-4003-9c83-7f86867ed759")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
